a18cec847234fdcd3771c82de6eaae70c5f34ef9
Aaron Mader <aaron@litiks.com>
2022-03-29 12:10:10 -0400

    Bugfix for django4. This has NOT been tested on older versions of django.

--- layoutform/templatetags/layoutform.py
+++ layoutform/templatetags/layoutform.py
@@ -67,7 +67,7 @@ def layout(obj):
 
             rows.append("<div>")
             for choice in field:
-                rows.append(u"<div class='radio'><label>%s %s</label></div>" % (choice.tag, choice.choice_label))
+                rows.append(u"<div class='radio'><label>%s %s</label></div>" % (choice.tag(), choice.choice_label))
 
             for error in field.errors:
                 rows.append(u"<span class='help-block %s'>%s</span>" % (error_css_class, error))
